87f4b30e4fe540ae482489294c027f10260e91bc
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-02-22 23:29:19 +0100

    first commit full stack commit

---
+++ frontend/src/components/Landing_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const LandingPage = () => (
+    <>
+    {/* <div className='landing_page_container'>
+            <Card
+                image='/assets/images/elliot.jpg'
+                header='Welcome'
+                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+    </div> */}
+    </>
+)
+export default LandingPage
